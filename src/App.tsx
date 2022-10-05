d618d56b4dbd5e9a874752b3f5b97674ed883212
Gene Zhang <genezhang@gmail.com>
2022-10-05 13:52:12 -0700

    working drag drop events + memoized List Row Items

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { DragEventHandler, memo, useCallback, useState } from 'react';
 import './App.css';
 import { faker } from '@faker-js/faker';
 import generateAddresses, { Address } from './utils/addresses';
@@ -17,18 +17,17 @@ const DEFAULT_INDENT = true
 const LEN = 100
 const addresses: Address[] = generateAddresses(LEN) // TODO: include indentation
 
+/** item properties
+ * 
+ * look up index by key => fast because we need to render those drop zones on hover
+ * 
+ */
 const items: Item[] = Array(LEN).fill(null).map((_, i) => {
-   const numAddr = addresses[i].numAddress // shorthand 
-   const borderTop = i === 0 || numAddr.length > addresses[i - 1].numAddress.length// shorter address => less indent => more exposed
-   const borderBottom = i === addresses.length - 1 || numAddr.length > addresses[i + 1].numAddress.length
    return {
       label: faker.hacker.phrase(),
       address: addresses[i].strAddress,
       numAddress: addresses[i].numAddress,
       itemKey: "" + i,
-      // note: currently unused in favor of margin-top hack (see below)
-      borderTop,
-      borderBottom,
    }
 })
 
@@ -37,48 +36,29 @@ type Item = {
    address: string,
    numAddress: number[],
    itemKey: string,
-   // no longer needed with negative margin hack
-   borderTop?: boolean,
-   borderBottom?: boolean,
 }
 
 type DisplayType = 'prefix' | 'suffix' | 'hidden'
 
-// TODO: Make draggable
-const ListRowItem = memo(({ item, displayType, indent }: {
-   item: Item
-   displayType?: DisplayType
-   indent: number
-   label?: string // allows container to deal with displayType, indentation, etc?
-}) => {
-   console.log(`List Items rendered: ${++listItemRenders}`)
+/** properties:
+ * 
+ * - doesn't re-render when order changes
+ * - only re-render when:
+ *   - label changes
+ *   - 
+ */
+type DragDropHandlerType = 'onDragStart' | 'onDragEnd' | 'onDrop' | 'onDragEnter';
+// TODO: consider implementing as enum?
+// const enum DragDropCallbacks {
+//    DragStart = 'onDragStart',
+//    DragEnd = 'onDragEnd',
+//    Drop = 'onDrop',
+//    DragEnter = 'onDragEnter',
+// }
 
-   const label = displayType === 'hidden' ? <span>{item.label}</span>
-      : displayType === 'prefix' ? <><span className="pr-1 text-neutral-400 float-left">{item.address + " - "}</span> {item.label}</>
-         : displayType === 'suffix' ? <>{`${item.label}`} <span className="pl-1 text-neutral-400">{`( ${item.address} )`}</span></>
-            : 'INVALID'
+type DragDropCallback = (itemKey: string, eventType: string, ev: Event) => void
 
-   // const simpleLabel = displayType === 'hidden' ? `${item.label}`
-   //    : displayType === 'prefix' ? `${item.address} - ${item.label}`
-   //       : displayType === 'suffix' ? `${item.label} ( ${item.address} )`
-   //          : 'INVALID'
 
-   const indentLevels = [0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
-   const indentLvl = indentLevels[Math.min(indent, indentLevels.length - 1)]
-   return (
-      <div
-         key={item.itemKey}
-         className={`p-1 pl-8 dark:border-neutral-600 text-left justify-start border-y-neutral-200 border-y-2 border-solid border-2 `}
-         style={{
-            marginLeft: `${indentLvl * 0.25}em`,// mL because"ml-N" tailwind utility classes aren't working...
-            marginTop: "-0.125em" // negative mT hack to avoid need to individually choose borderTop & borderBottom
-            // source: https://stackoverflow.com/questions/8358134/partial-border-in-css
-         }}
-      >
-         {label}
-      </ div >
-   )
-})
 
 function App() {
    // display options
@@ -114,27 +94,82 @@ function App() {
    );
 }
 
-function RegularList({ items, displayType, isIndenting }: {
+function RegularList({ items, displayType, isIndenting, ...props }: {
    items: Item[]
    displayType: DisplayType
    isIndenting: boolean
 }) {
-   return <div key="list" className="List">
-      {items.map((item, i) => (
+
+   const dragDropCallback: DragDropCallback = useCallback((itemKey, dndEventType, ev) => {
+      console.log(`${dndEventType} event for key ${itemKey}`, ev)
+   }, [])
+
+   const itemList =
+      items.map((item, i) => (
          <ListRowItem
             key={item.itemKey}
             item={item}
             displayType={displayType}
             indent={!isIndenting ? 0 : item.numAddress.length - 1}
+            dndCallbacks={['onDragStart', 'onDragEnter', 'onDragEnd']}
+            dragDropCallback={dragDropCallback}
          />
-      ))}
-   </div>
+      ))
+
+   // TODO: insert drop zones
+   return <div key="list" className="List">{itemList}</div>
 }
 
-function AutoReactWindowList({ items, displayType, isIndenting }: {
+
+const ListRowItem = memo(({ item, displayType, indent, ...props }: {
+   item: Item
+   displayType?: DisplayType
+   indent: number
+   label?: string // allows container to deal with displayType, indentation, etc?
+   dndCallbacks: DragDropHandlerType[]
+   dragDropCallback: DragDropCallback
+}) => {
+   console.log(`List Items rendered: ${++listItemRenders}`)
+
+   const label = displayType === 'hidden' ? <span>{item.label}</span>
+      : displayType === 'prefix' ? <><span className="pr-1 text-neutral-400 float-left">{item.address + " - "}</span> {item.label}</>
+         : displayType === 'suffix' ? <>{`${item.label}`} <span className="pl-1 text-neutral-400">{`( ${item.address} )`}</span></>
+            : 'INVALID'
+
+   const indentLevels = [0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
+   const indentLvl = indentLevels[Math.min(indent, indentLevels.length - 1)]
+   return (
+      <div
+         key={item.itemKey}
+         className={`p-1 pl-8 dark:border-neutral-600 text-left justify-start border-y-neutral-200 border-y-2 border-solid border-2 `}
+         draggable
+         style={{
+            marginLeft: `${indentLvl * 0.25}em`,// mL because"ml-N" tailwind utility classes aren't working...
+            marginTop: "-0.125em" // negative mT hack to avoid need to individually choose borderTop & borderBottom
+            // source: https://stackoverflow.com/questions/8358134/partial-border-in-css
+         }}
+         // attach the same callback for each requested drag drop event type
+         {...(props.dndCallbacks.reduce(
+            (obj, evType: DragDropHandlerType) => {
+               obj[evType] = (ev: Event) => {
+                  props.dragDropCallback(item.itemKey, evType, ev)
+                  // ev.preventDefault() // this will prevent associated enter events
+               };
+               return obj
+            }
+            , {} as any)
+         )}
+      >
+         {label}
+      </ div >
+   )
+})
+
+function AutoReactWindowList({ items, displayType, isIndenting, ...props }: {
    items: Item[]
    displayType: DisplayType
    isIndenting: boolean
+   dragDropCallback: DragDropCallback
 }) {
    return (<AutoSizer>
       {({ height, width }) => (
@@ -151,6 +186,8 @@ function AutoReactWindowList({ items, displayType, isIndenting }: {
                   item={items[index]}
                   displayType={displayType}
                   indent={!isIndenting ? 0 : items[index].numAddress.length - 1}
+                  dndCallbacks={[]}
+                  dragDropCallback={props.dragDropCallback}
                />
             )}
          </FixedSizeList>
